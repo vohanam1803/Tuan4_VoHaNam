21dd30b24c60f4f6d427506d1d35de4549700135
HP <HP@LAPTOP-MB0PCOPJ>
2022-03-16 15:45:22 +0700

    Add project files.

---
+++ Tuan4_VoHaNam/Models/Giohang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tuan4_VoHaNam.Models
+{
+    public class Giohang
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public int masach { get; set; }
+        [Display(Name = "Tên Sách")]
+        public string tensach { get; set; }
+        [Display(Name = "Ảnh bìa")]
+        public string hinh { get; set; }
+        [Display(Name = "Giá bán")]
+        public double giaban { get; set; }
+        [Display(Name = "Số lượng")]
+        public int iSoluong { get; set; }
+        [Display(Name = "Thành tiền")]
+        public double dThanhtien { get { return iSoluong * giaban; } }
+
+        public Giohang(int id)
+        {
+            masach = id; 
+            Sach sach = data.Saches.Single(n => n.masach == masach);
+            tensach = sach.tensach; 
+            hinh = sach.hinh;
+            giaban = double.Parse(sach.giaban.ToString()); 
+            iSoluong = 1;
+        }
+    }
+}
